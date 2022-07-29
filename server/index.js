a5fee8988146a64e82b6cb8b6c7797f728428c4c
Tim Chiang <tychiang922@gmail.com>
2022-07-29 22:34:28 +0000

    added server get command to yelp

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+'use strict';
+const yelp = require('yelp-fusion');
+const client = yelp.client('hQ9kH-rRd1q7AZRXFbMsJDLWlFrA_UpNw4NW6rHAq4eksdlkNrwQE0iwnWQ704F-3Or8Hc0y6eUOJN2WyM95NP-1AJqu8bwQrxM7szpPocEsuvXt3qyUEK4D4PDaYnYx');
 require('dotenv/config');
 const path = require('path');
 const express = require('express');
@@ -14,6 +17,20 @@ if (process.env.NODE_ENV === 'development') {
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+app.get('/api/nearby', (req, res, next) => {
+  client.search({
+    term: 'restaurant',
+    latitude: 33.6349171,
+    longitude: -117.7405465,
+    radius: 1000
+  })
+    .then(res => res.json(res))
+    .then(data => res.json(data))
+    .catch(e => {
+      res.status(404);
+    });
 });
 
 app.use(errorMiddleware);
